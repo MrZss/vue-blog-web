5deb3c5cf8e3ee5d0b6f0a6683da20ec92d57355
查辰昊 <zhachxfs@xinfushe.com>
2018-08-31 22:50:31 +0800

    头部导航

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+      }
+    },
+    created() {
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.header{
+  position: absolute;
+  margin: 0 auto;
+}
+</style>
